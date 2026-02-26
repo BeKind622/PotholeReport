d1b61167694f66e1bb7d5da86774bea9bfeeceaf
BeKind622 <116099798+BeKind622@users.noreply.github.com>
2026-02-26 13:56:36 +0000

    backup before pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {enabled: true}}),
+
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
